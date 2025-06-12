5313ac11d64e449ee5fc70532cc3d45405ff017e
ludovc <ludovcasu@gmail.com>
2025-06-12 17:11:25 +0200

    sd00/ex02

--- sd00/ex02/segmented_runs.c
+++ sd00/ex02/segmented_runs.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 int	check_segment(const int *arr, int size)
 {
 	int		i;
@@ -17,28 +16,41 @@ int	check_segment(const int *arr, int size)
 
 int	count_segments(const int *arr, int size)
 {
-	int		res;
+	int		i;
 	int		size_segment;
 	int		start_segment;
+	int		res;
 
-	(void)size;
 	res = 0;
+	size_segment = 0;
 	start_segment = 0;
-	size_segment = 3;
-	if (check_segment(&arr[start_segment], size_segment))
-		res++;
-	start_segment = 4;
-	size_segment = 4;
-	if (check_segment(&arr[start_segment], size_segment))
-		res++;
+	i = 0;
+	while (i < size + 1)
+	{
+		if (arr[i] == -1 || i == size)
+		{
+			if (check_segment(&arr[start_segment], size_segment))
+				res++;
+			start_segment = i + 1;
+			size_segment = 0;
+		}
+		else
+		{
+			size_segment++;
+		}
+		i++;
+	}
 	return (res);
 }
 
 #include <stdio.h>
 int	main()
 {
-	//int		arr[11] = {2, 3, 4, -1, 5, 6, -1, 1, 2, 3, 4};
-	int		arr[8] = {2, 3, 4, -1, 5, 1, 2, 3};
+	int		arr[11] = {2, 3, 4, -1, 5, 6, -1, 1, 2, 3, 4};
+	//int		arr[8] = {2, 3, 4, -1, 5, 1, 2, 3};
+	//int		arr[4] = {2, 3, 5, -1};
+	//int		arr[4] = {-1, 3, 5, 6};
+	//int		arr[9] = {2, 3, 5, -1, -1, 4, 2, 3, 5};
 
-	printf("%d\n", count_segments(arr, 8));
+	printf("%d\n", count_segments(arr, 11));
 }
