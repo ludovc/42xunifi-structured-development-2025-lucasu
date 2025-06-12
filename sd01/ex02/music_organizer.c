e8e1d9ef2649a05f807ff68b717804a069aab6ba
ludovc <ludovcasu@gmail.com>
2025-06-12 12:00:38 +0200

    sd01/ex02 wip

--- sd01/ex02/music_organizer.c
+++ sd01/ex02/music_organizer.c
@@ -2,5 +2,19 @@
 
 struct MusicLibrary	*organize_music_library(const char *directory_path)
 {
-	
+	struct MusicLibrary	*library;
+	int					i;
+	struct MusicFile	*song;
+	const char			**filenames;
+
+	library = create_music_library();
+	filenames = scan_directory(directory_path);
+	i = 0;
+	while (filenames[i])
+	{
+		song = process_music_file(directory_path, filenames[i]);
+		update_music_library(library, song);
+		i++;
+	}
+	return (library);
 }
